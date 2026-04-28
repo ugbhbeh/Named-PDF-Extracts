04edf7922bb74b48d76628d193927e941f12ad8a
root <187004982+ugbhbeh@users.noreply.github.com>
2026-04-28 05:31:04 +0000

    multiple inputs

--- scripts/splitting.py
+++ scripts/splitting.py
@@ -1,18 +1,30 @@
 from pypdf import PdfReader, PdfWriter
 import os
 
-input_pdf = "../input/input.pdf"
+input_dir = "../input"
 output_dir = "../generated/split&named"
 
 os.makedirs(output_dir, exist_ok=True)
 
-reader = PdfReader(input_pdf)
 
-for i, page in enumerate(reader.pages):
-    writer = PdfWriter()
-    writer.add_page(page)
+for filename in os.listdir(input_dir):
+    if filename.lower().endswith(".pdf"):
 
-    output_path = os.path.join(output_dir, f"page_{i + 1}.pdf")
+        input_path = os.path.join(input_dir, filename)
+        reader = PdfReader(input_path)
 
-    with open(output_path, "wb") as f:
-        writer.write(f)
+        base_name = os.path.splitext(filename)[0]
+
+        for i, page in enumerate(reader.pages):
+            writer = PdfWriter()
+            writer.add_page(page)
+
+            output_path = os.path.join(
+                output_dir,
+                f"{base_name}_page_{i + 1}.pdf"
+            )
+
+            with open(output_path, "wb") as f:
+                writer.write(f)
+
+        print(f"Split: {filename}")
